879fea85d9a2c5c016bf9082b3cc76beb5fe7a33
Siddharth Jain <85589093+sjain166@users.noreply.github.com>
2023-03-24 03:56:39 -0700

    Layout Setup

--- app/src/main/java/com/example/image_detector/MainActivity.kt
+++ app/src/main/java/com/example/image_detector/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.image_detector
 
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bitmap: Bitmap
+    lateinit var imgView : ImageView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imgView = findViewById(R.id.imageView);
+
+        var selectButton:Button = findViewById(R.id.selectB)
+
+        selectButton.setOnClickListener(View.OnClickListener {
+
+            var intent: Intent = Intent(Intent.ACTION_GET_CONTENT);
+            intent.type = "image/*"
+            startActivity(intent);
+        })
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
